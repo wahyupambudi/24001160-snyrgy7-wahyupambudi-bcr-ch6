5cd67bf52000cdc4632454266da59fea0d6fd9cd
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-05-29 15:43:28 +0700

    feat(bcr) - add Role for User

--- migrations/20240527132338_users.ts
+++ migrations/20240527132338_users.ts
@@ -6,6 +6,7 @@ export async function up(knex: Knex): Promise<void> {
         table.string('name').notNullable();
         table.string('email').unique();
         table.string('password');
+        table.string('role').defaultTo('member');
         table.timestamp('createdAt').defaultTo(knex.fn.now());
         table.timestamp('updatedAt').defaultTo(knex.fn.now());
     });
--- seeds/users.ts
+++ seeds/users.ts
@@ -6,7 +6,8 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("users").insert([
-        { name: "rowValue1", email: "email1@example.com", password: "password1", createdAt: knex.fn.now(), updatedAt: knex.fn.now() },
-        { name: "rowValue2", email: "email2@example.com", password: "password2", createdAt: knex.fn.now(), updatedAt: knex.fn.now() },
+        { name: "rowValue1", email: "email1@example.com", password: "password1", role: "superadmin", createdAt: knex.fn.now(), updatedAt: knex.fn.now() },
+        { name: "rowValue2", email: "email2@example.com", password: "password2", role: "admin", createdAt: knex.fn.now(), updatedAt: knex.fn.now() },
+        { name: "rowValue3", email: "email3@example.com", password: "password3", role: "member", createdAt: knex.fn.now(), updatedAt: knex.fn.now() },
     ]);
 };
