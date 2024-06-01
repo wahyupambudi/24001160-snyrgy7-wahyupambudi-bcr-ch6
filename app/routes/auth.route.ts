b02413bb6c612a44cb67af4fac67994d57b7b710
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-06-01 15:20:10 +0700

    feat(bcr) - add route for auth

---
+++ app/routes/auth.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import authController from "../controller/api/v1/authController";
+import { Authenticate } from "../middlewares/authentication";
+
+const router = Router();
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/whoami", Authenticate, authController.whoami);
+
+export default router;
