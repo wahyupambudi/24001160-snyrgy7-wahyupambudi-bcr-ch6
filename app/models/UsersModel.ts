f7967c2ba3b8d98cdc85bdfb98c54ff41d74e0e9
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-06-01 15:25:06 +0700

    feat(bcr) - add models user role

--- app/models/UsersModel.ts
+++ app/models/UsersModel.ts
@@ -4,6 +4,7 @@ export class UsersModel extends Model {
     id!: number;
     name!: string;
     email!: string;
+    role!: string;
     password?: string;
     createdAt!: Date;
     updatedAt!: Date;
