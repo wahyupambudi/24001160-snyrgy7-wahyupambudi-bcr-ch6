0c92572911cefe097bc60669d8f12ab189833838
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-06-01 15:28:01 +0700

    feat(bcr) - add env for upload cloudinary

--- app/utils/uploadCloudinary.ts
+++ app/utils/uploadCloudinary.ts
@@ -1,10 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
+const cloudinaryConfig = require('../config/cloudinary')
 
 export const uploadToCloudinary = async (fileBuffer: Buffer, mimetype: string, folder: string) => {
     const fileBase64 = fileBuffer.toString('base64');
     const file = `data:${mimetype};base64,${fileBase64}`;
 
-    const result = await cloudinary.uploader.upload(file, {
+    const result = await cloudinaryConfig.uploader.upload(file, {
         folder: folder,
         use_filename: true,
     });
