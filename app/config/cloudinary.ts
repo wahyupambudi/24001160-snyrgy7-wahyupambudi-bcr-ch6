1c385aa877894f3d9551f1f0577ec2e24ea80e88
Wahyu Pambudi <wahyupambudi823@gmail.com>
2024-06-01 15:23:15 +0700

    feat(bcr) - add ; wkwk

--- app/config/cloudinary.ts
+++ app/config/cloudinary.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
     secure: true
 });
 
-module.exports = cloudinary
+module.exports = cloudinary;
